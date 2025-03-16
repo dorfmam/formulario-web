6bd559e3e5b38cdba1fa85eb75122b3a2ed2424c
Caio Dorfmam <dasdorfmam@gmail.com>
2025-03-16 04:22:45 -0300

    Removendo um arquivo não necessário para o funcionamento do código.

--- script.js
+++
@@ -1,9 +0,0 @@
-// Bloco de código para definir que a página seja carregada após o formulário ser submetido,
-
-const formulario = document.querySelector(".formulario");
-formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-    setTimeout(() => {
-        location.reload();
-    }, 2000);
-});
