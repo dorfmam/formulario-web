a78997a3358240d992daa4fe5999cb5fbef5c6af
Caio Dorfmam <dasdorfmam@gmail.com>
2025-03-21 11:40:50 -0300

    Adicionada melhorias no código

--- server.js
+++ server.js
@@ -3,33 +3,41 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-// Cria uma aplicação Express.js e define a porta como 3.000.
+// Cria uma aplicação Express.js e define a porta como 3000.
 const app = express();
 const PORT = 3000;
 
-// Utiliza o Middleware para permitir requisições CORS
+// Utiliza o Middleware para permitir requisições do tipo CORS
 app.use(cors());
 
-// Utiliza o Middleware para servir os arquivos "HTML5", "CSS3" e "JavaScript"
-app.use(express.static(path.join(__dirname, "public")));
-
-// Utiliza o Middleware para ler arquivos JSON
+// Utiliza o Middleware para ler arquivos JSON.
 app.use(express.json());
 
-// Permite receber os dados do formulário
+// Analisa através de um Middleware dados codificados em uma URL.
 app.use(express.urlencoded({ extended: true }));
 
-// Define uma função para lidar com os dados recebidos da aplicação front-end
-app.post("/submit", (req, res) => {
-    const { nome, email, idade, telefone } = req.body;
-    console.log("Dados recebidos: ", { nome, email, idade, telefone });
-
-    res.json({ mensagem: "Dados recebidos com sucesso!" });
-});
+// Utiliza o Middleware para servir os arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve o HTML, obtendo o arquivo "index.html"
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Define a rota POST para receber os cadastros
+app.post("/cadastro", (req, res) => {
+    const { nome, email, idade, telefone } = req.body;
+
+    // Verifica se todos os campos foram preenchidos pelo cliente
+    if (!nome || !email || !idade || !telefone) {
+        return res
+            .status(400)
+            .json({ message: "Todos os campos são obrigatórios" });
+    }
+
+    console.log("Novo cadastro enviado: ", req.body);
+
+    res.status(200).json({ message: "Cadastro realizado com sucesso!" });
 });
 
 // Executa a aplicação na porta 3000
