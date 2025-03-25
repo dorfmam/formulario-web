ede5de430cbd1c90ca5716fb9cf553b578e1072b
Caio Dorfmam <dasdorfmam@gmail.com>
2025-03-25 01:23:22 -0300

    Adicionando o site estático com redirecionamento.

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-// Importação os módulos necessários
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-// Cria uma aplicação Express.js e define a porta como 3000.
 const app = express();
 const PORT = 3000;
 
-// Utiliza o Middleware para permitir requisições do tipo CORS
 app.use(cors());
 
-// Utiliza o Middleware para ler arquivos JSON.
 app.use(express.json());
 
-// Analisa através de um Middleware dados codificados em uma URL.
 app.use(express.urlencoded({ extended: true }));
 
-// Utiliza o Middleware para servir os arquivos estáticos
 app.use(express.static(path.join(__dirname, "docs")));
 
-// Serve o HTML, obtendo o arquivo "index.html"
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "docs", "index.html"));
 });
 
-// Define a rota POST para receber os cadastros
 app.post("/cadastro", (req, res) => {
     const { nome, email, idade, telefone } = req.body;
 
-    // Verifica se todos os campos foram preenchidos pelo cliente
     if (!nome || !email || !idade || !telefone) {
         return res
             .status(400)
@@ -40,7 +31,6 @@ app.post("/cadastro", (req, res) => {
     res.redirect("/confirmed.html");
 });
 
-// Executa a aplicação na porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor executando em http://localhost:${PORT}`);
 });
